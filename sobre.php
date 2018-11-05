cb3306123bec4fb6b8114b1329163987394addf5
Gabriel Gasparino <gasparinogabriel2@gmail.com>
2018-11-05 17:19:19 -0200

    mexendo no sobre começando a fazer os cards com box-shadow

--- sobre.php
+++ sobre.php
@@ -14,15 +14,23 @@
 	<section class="main-sobre">
 		<div class="wrap">
 			<article>
-				<h2>Missão</h2>
+				<div class="destaque-sobre">
+					<h2>Missão</h2>
+				</div>
 				<p>Prestar serviços de Marketing Digital, destinado a usar recursos para divulgar ao máximo a empresa e até redes socais dos clientes, com confiabilidade e custos adequados a seus negócios.</p>
 			</article>
 			<article>
-				<h2>Visão</h2>
+				<div class="destaque-sobre">
+					
+					<h2>Visão</h2>
+				</div>
 				<p>Ser uma plataforma reconhecida, por sua resposabilidade e confiança, destacando-se no ramo de Marketing Digital, com sua equipe capaciatada e comprometida com a qualidade total e a satisfação dos clientes.</p>
 			</article>
 			<article>
+				<div class="destaque-sobre">
+					
 				<h2>Valores</h2>
+				</div>
 				<p>Compromisso,
 					Confiablidade,
 					Ética e transparência
@@ -40,17 +48,25 @@
 
 				<div class="apresentaPlanos">
 					<div class="planos">
-						<img src="img/google.svg" alt="">
+						<div class="destaque-sobre">
+							<img src="img/google.svg" alt="">
+						</div>
 						<h3>Adwords</h3>
 						<p>Para anúncios e Campanhas. Cuja abrangência traz ótimos resultados e possívéis novos clientes para serem fidelizados.</p>
 					</div>
 					<div class="planos">
+						<div class="destaque-sobre">
+							
 						<img src="img/facebook.svg" alt="">
+						</div>
 						<h3>Social Media</h3>
 						<p>Aproxima a marca do seu target, sendo efetivo ao trazer a marca para o dia-a-dia do seu cliente</p>
 					</div>
 					<div class="planos">
+						<div class="destaque-sobre">
+							
 						<img src="img/googleanalytics.svg" alt="">
+						</div>
 						<h3>Analytics</h3>
 						<p>Ferramenta análise e controle de relatórios acerca dos usuários. Sendo uma ferramenta primordial para acertar o target.</p>
 					</div>
@@ -58,12 +74,18 @@
 				</div>
 				<div class="apresentaPlanos">
 					<div class="planos">
-						<img src="img/google.svg" alt="">
+						<div class="destaque-sobre">
+							
+						<img src="img/seo.svg" alt="">
+						</div>
 						<h3>SEO</h3>
 						<p>Para anúncios e Campanhas. Cuja abrangência traz ótimos resultados e possívéis novos clientes para serem fidelizados.</p>
 					</div>
 					<div class="planos">
-						<img src="img/facebook.svg" alt="">
+						<div class="destaque-sobre">
+							
+						<img src="img/ad.svg" alt="">
+						</div>
 						<h3>Adsense</h3>
 						<p>Aproxima a marca do seu target, sendo efetivo ao trazer a marca para o dia-a-dia do seu cliente</p>
 					</div>
