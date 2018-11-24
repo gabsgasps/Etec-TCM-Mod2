188ec47e46e8fb6cc1dc4f7536b398b6b4902668
Gabriel Gasparino <gasparinogabriel2@gmail.com>
2018-11-24 13:18:18 -0200

    carde da seção de contato feito, ncessário aprovação de todos agora

--- contato.php
+++ contato.php
@@ -8,24 +8,34 @@
 	<div class="areaForm">
 		<div class="wrap">
             <div class="info-destaqueCont">
-                <i class="material-icons" style="font-size: 10rem; color: goldenrod; display:block; margin: 0 auto;">
+                <i class="material-icons" style="font-size: 10rem; color: goldenrod;text-align: center;">
                     mail
                 </i>
             <p>Se tiver dúvidas ou quiser apenas entrar em contato, use o formulario ao lado. Estamos ansiosos para ouvir de você</p>
             
-            <p> ttargeet@gmail.com</p>
             <p>
-                
+				<i class="material-icons">
+                    mail
+                </i>
+            ttargeet@gmail.com</p>
+            <p>
+			        <i class="material-icons">
+			local_phone
+			</i>
                 +55(11)94631-0146
             </p>
             <p>
-                
+				                <i class="material-icons">
+				phone_android
+				</i>
                 +55(11)96101-4583
             </p>
 
             </div>
             
 			<form action="enviar.php" method="post">
+				<h2 style="text-align: center; font-size: 2.5rem;
+				font-weight: bolder; margin-bottom: 2rem">Contato</h2>
 				<input type="text" placeholder="Nome" name="campoNome" class="campoEmail">
 				<input type="text" placeholder="Email" name="campoEmail" class="campoEmail">
 
